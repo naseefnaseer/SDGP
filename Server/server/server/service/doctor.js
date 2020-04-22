fce6add576224321940e3cdaac9faff2fc9f5aa0
naseernaseef <52096695+naseernaseef@users.noreply.github.com>
2020-04-22 17:07:27 +0530

    Backend component appended

---
+++ Server/server/server/service/doctor.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+
+var doctor = require('./../model/doctor');
+ 
+// Create new doctor
+exports.createDoctor = function (data, callback) {
+    doctor.create(data).then((response) => {
+        callback(null, response);
+    }, (error) => {
+        callback(error, null);
+    });
+};
+
+// Find doctor
+exports.findDoctor = function (query, callback) {
+    doctor.findOne(query, callback);
+}
+
+// Update doctor by ID
+exports.updateDoctorById = function (id, data, callback) {
+    doctor.findByIdAndUpdate({
+        doctorID: id
+    }, data, (err, response) => {
+        callback(err, response);
+    });
+}
+
+// Update doctor by Querry
+exports.updateDoctorByQuerry = function (query, data, options, callback) {
+    doctor.findOneAndUpdate(query, data, options, (err, response) => {
+        callback(err, response);
+    });
+}
+
+// Delete doctor by Querry
+exports.deleteDoctor = function (query, callback) {
+    doctor.deleteOne(query, callback);
+}
