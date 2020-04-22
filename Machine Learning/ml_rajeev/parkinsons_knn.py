a5a793752a5f8b6db53d7aeab037d3a948308903
Rajeev Kodippily <rkodippily@gmail.com>
2020-04-22 14:58:03 +0530

    KNN classifier getting 92% testing set accuracy

--- Machine Learning/ml_rajeev/parkinsons_knn.py
+++ Machine Learning/ml_rajeev/parkinsons_knn.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
-y = dataset.iloc[:, 4].values
+dataset = pd.read_csv('parkinsons_data.csv')
+X = dataset.iloc[:, 1: 23].values #matrix of independent features
+y = dataset.iloc[:, 23].values #dependent variable
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
@@ -21,15 +21,23 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # Fitting classifier to the Training set
-# Create your classifier here
+# Create your classifier here - knn
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
 
+#getting accuracy
+accuracy_score(y_test,y_pred)
+print("Accuracy_score for KNN classifier ", accuracy_score(y_test, y_pred))
+
+'''
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
@@ -65,3 +73,4 @@ plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
+'''
